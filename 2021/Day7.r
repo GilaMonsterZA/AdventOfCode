50e19a917665dc220521e31135a4f0b38d0001d4
Gail Shaw <gail@sqlinthewild.co.za>
2023-06-28 20:10:21 +0200

    Day 7 testing

--- 2021/Day7.r
+++ 2021/Day7.r
@@ -1,3 +1,5 @@
+library(tidyverse)
+
 Input <- '16,1,2,0,4,2,7,1,2,14'
 
 StartingPositions <- as.integer(str_split_1(Input, ','))
@@ -7,3 +9,11 @@ min(StartingPositions)
 
 PossiblePositions <- seq(from=min(StartingPositions), to=max(StartingPositions))
 FuelCosts <- integer(length(PossiblePositions))
+
+for (i in 1:17) {
+  FuelCosts[i] = sum(abs(StartingPositions-PossiblePositions[i]))
+}
+
+PossiblePositions
+FuelCosts
+
